4f9f2f3351d32453a053e26e06f89d11d6990f18
NDK1410 <khanh.nd1410@gmail.com>
2020-07-11 11:00:52 +0700

    development

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+import ToDoListScreen from './src/screens/ToDoListScreen'
+import HomeScreen from './src/screens/HomeScreen'
+import FormDataScreen from './src/screens/FormDataScreen'
+import { Provider } from 'react-redux'
+import store from './store'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    List: ToDoListScreen,
+    Form: FormDataScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <Provider store={store}>
+      <AppContainer />
+    </Provider>;
+  }
+}
---
+++ src/fetchAPIs/updateDataApi.js
@@ -0,0 +1,23 @@
+export default function updateDataApi(data) {
+  const dataContent = {
+    content: data.content
+  }
+  return new Promise((resolve, reject) => {
+
+    const url = 'http://192.168.1.6:3001/todos/' + data.id
+    fetch(url, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(dataContent)
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        console.log('res', res)
+        resolve(res);
+      })
+      .catch((error) => {
+        console.log('error', error)
+        reject(error);
+      });
+  });
+}
